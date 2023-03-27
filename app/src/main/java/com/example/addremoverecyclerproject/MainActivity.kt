6b7322fa546a28f9809a6e894ba3e842e8c3eaee
YuCheolHwan <cheolhwan0816@gmail.com>
2023-03-27 17:50:00 +0900

    [create] AddRemoveRecycler project created

---
+++ app/src/main/java/com/example/addremoverecyclerproject/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.addremoverecyclerproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.addremoverecyclerproject.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    lateinit var binding : ActivityMainBinding
+    lateinit var dataList : MutableList<DataVO>
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        dataList = mutableListOf<DataVO>()
+        binding.btnFAB.setOnClickListener(this)
+
+        for(i in 1..30){
+            if(i%2==0){
+                dataList.add(DataVO("홍길동$i", "${20+i}", "rlfehd$i@naver.com", R.drawable.man))
+            } else {
+                dataList.add(DataVO("홍길순$i", "${20+i}", "rlftns$i@naver.com", R.drawable.woman))
+            }
+
+        }
+        binding.recyclerView.adapter = CustomAdapter(dataList)
+        val layoutManager = LinearLayoutManager(this)
+        layoutManager.orientation = LinearLayoutManager.VERTICAL
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.setHasFixedSize(true)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btnFAB -> {
+                val dialog = CustomDialog(this,binding)
+                dialog.showDialog()
+            }
+        }
+    }
+fun refreshRecyclerView(dataVO: DataVO){
+    dataList.add(dataVO)
+    binding.recyclerView.adapter?.notifyDataSetChanged()
+    Toast.makeText(applicationContext,"${dataVO.tvName} 추가되었어요",Toast.LENGTH_SHORT).show()
+}
+}
